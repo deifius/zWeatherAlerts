f809e309d8bc14248435175d58bcb65e57bd725e
deifius <sunji.roaoul@gmail.com>
2022-02-11 05:15:59 -0700

    begun parsing NOAA zones, euxlidean distance f() for knearestneighbor

--- alertor.py
+++ alertor.py
@@ -12,7 +12,15 @@ https://alerts.weather.gov/cap/wwaatmget.php?x=MIC055&y=0
 ForecastZones can be k-nearest neighbored from ForecastZoneGeoLoc.txt
 """
 
+def eat_Forecast_Zones():
+	with open('ForecastZoneGeoLoc.txt') as stuff:  stats = stuff.read().split('\n')
+	for each_stat in enumerate(stats):
+		stats[each_stat[0]] = each_stat[1].split('|')
+	return stats
 
+def euclid_distance(point1, point2):
+	try: return sqrt((point1[0]-point2[0])**2+(point1[1]-point2[1])**2)
+	except: print('points not formatted correctly?')
 
 def checkTheWeather(**whereAmI):
 	# read samecodes every time and grab any alerts from NOAA
